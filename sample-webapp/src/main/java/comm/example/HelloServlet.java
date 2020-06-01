a8790cc36029daabc0cafca9f5f57ebd56d50a76
nilay <mailnilaydas@gmail.com>
2020-06-01 16:08:27 +0530

    added our first servlet

---
+++ sample-webapp/src/main/java/comm/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package comm.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h3> hello world servlet</h3>");
+		out.println("welcome to the world of J2EE");
+	}
+
+}
