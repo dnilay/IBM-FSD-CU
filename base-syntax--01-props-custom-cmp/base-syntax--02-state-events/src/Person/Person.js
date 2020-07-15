ebb2628c1ef42a40937e28a0003d2c390fa35740
Nilay <54245048+dnilay@users.noreply.github.com>
2020-07-15 15:37:53 +0530

    Add files via upload

---
+++ base-syntax--01-props-custom-cmp/base-syntax--02-state-events/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = ( props ) => {
+    return (
+        <div>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
+
+export default person;
